d723d7c9bee4fd2a4272749ceeef1f49606390bf
RediG7 <guleksiredi7@gmail.com>
2021-10-10 00:25:18 +0200

    Initial Commit

---
+++ PMToolBackend/src/test/java/com/guleksiredi/pmtool/PmToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guleksiredi.pmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
