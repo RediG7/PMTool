b0157d2e8fef01b2612aa6f6ace4643de254ca14
RediG7 <guleksiredi7@gmail.com>
2021-10-10 00:53:53 +0200

    Project Object and Repo

---
+++ PMToolBackend/src/main/java/com/guleksiredi/pmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.guleksiredi.pmtool.repositories;
+
+import com.guleksiredi.pmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> longs);
+}
