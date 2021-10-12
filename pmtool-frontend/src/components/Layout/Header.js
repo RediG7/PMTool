d6db188011c5a8d4d21a15657f60cb858a7df88e
RediG7 <guleksiredi7@gmail.com>
2021-10-13 00:24:48 +0200

    Initial Style for Dashboard, Navbar, ProjectItem

--- pmtool-frontend/src/components/Layout/Header.js
+++ pmtool-frontend/src/components/Layout/Header.js
@@ -1,11 +1,47 @@
 import React, { Component } from "react";
+import "./Header.css";
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <a className="navbar-brand" href="">
+            Project Management Tool
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse rightSide" id="mobile-nav">
+            <ul className="navbar-nav mr-auto ">
+              <li className="nav-item">
+                <a className="nav-link" href="">
+                  Dashboard
+                </a>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link " href="">
+                  Sign Up
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
